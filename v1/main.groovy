f16a3ac21e9ae411f9cf01f5078552585f253160
Edson Marquezani Filho <edsonmarquezani@gmail.com>
2017-08-13 00:34:10 -0300

    .

--- v1/main.groovy
+++ v1/main.groovy
@@ -1,41 +1,50 @@
 try {
 
-  node {
+  groovy_dir = "/tmp/${env.BUILD_TAG}"
 
-    // Code checkout
-    stage("Code Checkout") {
-      checkout changelog: false,
-               poll: false,
-               scm: [
-                 $class: 'GitSCM',
-                 branches: [[name: '*/master']],
-                 doGenerateSubmoduleConfigurations: false,
-                 userRemoteConfigs: [
-                   [ credentialsId: 'git',
-                     url: 'git@github.com:edsonmarquezani/jenkins.git'
-                   ]
+  // Code checkout
+  stage("Init") {
+    checkout changelog: false,
+             poll: false,
+             scm: [
+               $class: 'GitSCM',
+               branches: [[name: '*/master']],
+               doGenerateSubmoduleConfigurations: false,
+               extensions: [[
+                 $class: 'RelativeTargetDirectory',
+                 targetDir: groovyDir
+               ]],
+               userRemoteConfigs: [
+                 [ credentialsId: 'git',
+                   url: 'git@github.com:edsonmarquezani/jenkins.git'
                  ]
                ]
-    }
+             ]
+    checkout scm
+  }
 
-    // Reads Pipeline meta-config
-    configObject = readYaml file: 'pipeline.yml'
+  // Reads Pipeline meta-config
+  config_object = readYaml file: 'pipeline.yml'
 
+  node(config_object.node_type) {
     // Iterates main 'stages' list and launchs every stage defined invoking its
     // defined module
     def i=0
-    for (i=0; i < configObject.stages.size(); i++) {
+    for (i=0; i < config_object.stages.size(); i++) {
 
       // Getting stage
       stage_name = configObject.stages[i].name
+      module_name = configObject.stages[i].module
+      submodule_name = configObject.stages[i].submodule
       module_opts = configObject.stages[i].options
+
       // module path includes the API version declared in the config, along
       // with module/submodule
-      module_path = "${configObject.version}/modules/${configObject.stages[i].module}.groovy"
+      module_path = "${configObject.version}/modules/${module_name}/${submodule_name}.groovy"
 
       // Declares the stage
       stage(stage_name) {
-         evaluate(readFile(module_path))
+        evaluate(readFile("${groovy_dir}/module_path"))
       }
     }
   }
