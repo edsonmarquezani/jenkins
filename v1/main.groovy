50f99ed9957c20f9d1e35702fb5d797c78c90b1e
Edson Marquezani Filho <edsonmarquezani@gmail.com>
2017-08-13 23:47:08 -0300

    .

--- v1/main.groovy
+++ v1/main.groovy
@@ -1,53 +1,61 @@
-try {
+  try {
 
-  groovy_dir = "/tmp/${env.BUILD_TAG}"
+  def modules = []
+  def config_object
 
-  // Code checkout
-  stage("Init") {
-    checkout changelog: false,
-             poll: false,
-             scm: [
-               $class: 'GitSCM',
-               branches: [[name: '*/master']],
-               doGenerateSubmoduleConfigurations: false,
-               extensions: [[
-                 $class: 'RelativeTargetDirectory',
-                 targetDir: groovyDir
-               ]],
-               userRemoteConfigs: [
-                 [ credentialsId: 'git',
-                   url: 'git@github.com:edsonmarquezani/jenkins.git'
+  node {
+    // Clean-up workspace
+    deleteDir()
+    // Code checkout
+    stage("Init Modules") {
+      checkout changelog: false,
+               poll: false,
+               scm: [
+                 $class: 'GitSCM',
+                 branches: [[name: '*/master']],
+                 doGenerateSubmoduleConfigurations: false,
+                 userRemoteConfigs: [
+                   [ credentialsId: 'git',
+                     url: 'git@github.com:edsonmarquezani/jenkins.git'
+                   ]
                  ]
                ]
-             ]
-    checkout scm
+    }
+
+    // Reads Pipeline meta-config
+    config_object = readYaml file: "pipeline.yml"
+
+    // Load modules to be use in the following stages
+    for (i=0; i < config_object.stages.size(); i++) {
+      module_name = config_object.stages[i].module
+      module_path = "${config_object.version}/modules/${module_name}.groovy"
+      module = load module_path
+      modules[i] = module
+    }
   }
 
-  // Reads Pipeline meta-config
-  config_object = readYaml file: 'pipeline.yml'
-
   node(config_object.node_type) {
+
+    stage("Code Checkout") {
+      // Checkout source code
+      checkout scm
+    }
+
     // Iterates main 'stages' list and launchs every stage defined invoking its
     // defined module
-    def i=0
     for (i=0; i < config_object.stages.size(); i++) {
 
       // Getting stage
-      stage_name = configObject.stages[i].name
-      module_name = configObject.stages[i].module
-      submodule_name = configObject.stages[i].submodule
-      module_opts = configObject.stages[i].options
-
-      // module path includes the API version declared in the config, along
-      // with module/submodule
-      module_path = "${configObject.version}/modules/${module_name}/${submodule_name}.groovy"
+      stage_name = config_object.stages[i].name
+      module_opts = config_object.stages[i].options
 
       // Declares the stage
       stage(stage_name) {
-        evaluate(readFile("${groovy_dir}/module_path"))
+        modules[i].main()
       }
     }
   }
+
 } catch(exec) {
   err = caughtError
   currentBuild.result = "FAILURE"
